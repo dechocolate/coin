2e41d4036e2e8637ba1c7481fa70076eed996244
june <kimjy4536@hotmail.com>
2017-12-19 10:51:13 +0900

    no message

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ var coins = {};
 // 'XLM', 'BCC', 'MER', 'VOX', 'EMC2', 'SHIFT', 'NEO', 'OMG', 'STORJ', 'KMD', 'TIX', 'REP'];
 
 // var type = ['BTC', 'ADA', 'XRP', 'ETH', 'ETC', 'QTUM', 'BCC', 'KMD', 'TIX', 'EMC2', 'LSK'];
-var type = ['BTC', 'XLM', 'NEO', 'XEM', 'ETC', 'LSK'];
+var type = ['BTC', 'XLM', 'NEO', 'XEM', 'LSK', 'EMC2', 'ADA', 'XMR'];
 
 function Coin(name, url, pre, per, max, min, now, init) {
 	this.name = name;
